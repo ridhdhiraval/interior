4d9cc1af654828d212edbd30836049fc7fb2fd2e
Ridhdhi Raval <ridhdhiraval2005@gmail.com>
2026-02-12 18:25:49 +0530

    solve the conflict

--- src/components/PlannerCanvas.jsx
+++ src/components/PlannerCanvas.jsx
@@ -1,29 +1,3 @@
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import { plannerState } from "../planner/plannerState";
-
-export default function PlannerCanvas() {
-  return (
-    <Canvas camera={{ position: [6, 6, 6] }}>
-      <ambientLight intensity={0.6} />
-      <directionalLight position={[5, 10, 5]} />
-
-      {/* Floor */}
-      <mesh rotation={[-Math.PI / 2, 0, 0]}>
-        <planeGeometry args={[20, 20]} />
-        <meshStandardMaterial color="#eaeaea" />
-      </mesh>
-
-      {/* Walls */}
-      {plannerState.walls.map((w, i) => (
-        <mesh key={i}>
-          <boxGeometry args={[5, w.height, 0.2]} />
-          <meshStandardMaterial color="#fff" />
-        </mesh>
-      ))}
-
-      <OrbitControls />
-=======
 import React, { useRef, useState, useMemo } from 'react';
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Grid, Text, Edges, MapControls } from "@react-three/drei";
@@ -263,7 +237,6 @@ export default function PlannerCanvas() {
         minZoom={10}
         maxZoom={100}
       />
->>>>>>> bbfc30066be1832d95eae055ffff91da37ce73ce
     </Canvas>
   );
 }
